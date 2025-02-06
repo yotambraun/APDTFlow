0f91c84f0fd25ad37ed6e5db85574f439db8c0f3
yotam <yotambarun93@gmail.com>
2025-02-06 10:56:32 +0200

    update the setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='apdtflow',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'torch',
@@ -14,12 +19,15 @@ setup(
         'pyyaml',
     ],
     description='APDTFlow: A modular forecasting framework for time series data',
+    long_description=long_description, 
+    long_description_content_type="text/markdown", 
     author='Yotam Barun',
     author_email='yotambarun93@gmail.com',
-    url='https://github.com/yotambraun/APDTFlow',  # Main homepage URL
+    url='https://github.com/yotambraun/APDTFlow',
     project_urls={
         'Homepage': 'https://github.com/yotambraun/APDTFlow',
         'Documentation': 'https://github.com/yotambraun/APDTFlow',
         'Source': 'https://github.com/yotambraun/APDTFlow',
     },
 )
+
